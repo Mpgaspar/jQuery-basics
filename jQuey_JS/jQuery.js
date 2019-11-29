88bf80a0ae7d01d01d8baef02c5bf968378ac64a
Marcelo Gaspar <drmpgaspar@gmail.com>
2019-11-29 17:01:29 +0100

    Create the basics events of jquery

--- jQuey_JS/jQuery.js
+++ jQuey_JS/jQuery.js
@@ -1,15 +1,46 @@
-$(document).ready(function(){
+//Hello World
+/*$(document).ready(function(){
     alert("Hello World!");		
+  });*/
+
+$.when($.ready).then(function() {
+// Document is ready.
   });
 
-  $.when( $.ready ).then(function() {
-  // Document is ready.
-  });
-
-  $( "#target" ).click(function() {
-  alert( "Handler for .click() called." );
+//Click
+$("#target").click(function() {
+  alert( "thanks for clicking" );
 });
 
-$( "#other" ).click(function() {
-    $( "#target" ).click();
+//Hover
+$(document).ready(function(){
+  $("#target1").hover(function(){
+    $(this).css("background-color", "yellow");
+    }, function(){
+    $(this).css("background-color", "pink");
   });
+});
+
+//Keypress
+$(document).ready(function(){
+	$("#target2").keydown(function(event){
+		$("#key").text("The key code of " + String.fromCharCode(event.which) + " is: " + event.which);
+		$("#target2").val("");
+	}); 
+});
+
+//Cursor position
+$(document).ready(function(){
+  $(document).mousemove(function(event){
+    $("span").text(event.pageX + ", " + event.pageY);
+  });
+});
+
+//Event to many elements
+$('#elm1, #elm2').click(function() {
+  alert("thanks for clicking");
+});
+
+
+
+
