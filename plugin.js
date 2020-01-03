aa4c4ce77d2b6ef305fdc21598c10d41868fd248
Marcelo Gaspar <drmpgaspar@gmail.com>
2020-01-03 18:09:56 +0100

    Create and test plugin

---
+++ plugin.js
@@ -0,0 +1,47 @@
+card = $("#user");
+card02 = $('#user02');
+
+$("head").append(`<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">`)
+
+card.append(`<div class="container col s12 m4 l4">
+<div class="card horizontal">
+  <div class="card-image">
+    <img src="http://lorempixel.com/100/190/business/10/">
+  </div>
+  <div class="card-stacked">
+    <div class="card-content">
+      <h5 class="grey-text">Jackie Chan</h5>
+      <p>App Designer & Graphic Designer<p>
+    </div>
+    <div class="card-action grey">
+      <a href="#"><img src="assets/facebook.png"></img></a>
+      <a href="#"><img src="assets/instagram.png"></img></a>
+      <a href="#"><img src="assets/twitter.png"></img></a>
+    </div>
+  </div>
+</div>
+</div><br>`)
+
+card02.append(`<div class="container col s12 m4 l4">
+<div class="card horizontal">
+  <div class="card-image">
+  <img src="http://lorempixel.com/100/190/people/9/"> 
+  </div>
+  <div class="card-stacked">
+    <div class="card-content">
+      <h5 class="grey-text">Chun-Li</h5>
+      <p>Full Stack Web Developer<p>
+    </div>
+    <div class="card-action grey">
+    <a href="#"><img src="assets/facebook.png"></img></a>
+    <a href="#"><img src="assets/instagram.png"></img></a>
+    <a href="#"><img src="assets/twitter.png"></img></a>
+    </div>
+  </div>
+</div>
+</div>`)
+
+
+
+
+ 
